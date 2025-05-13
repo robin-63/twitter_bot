76ca05aef2b744a1d390152df5d89bf0b7e68ced
robin-63 <122930015+robin-63@users.noreply.github.com>
2025-05-13 19:17:48 -0400

    Twitter_bot
    Twitter_bot

---
+++ config.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = "7758431818:AAE9DX6GgFcEVT9XWv-rlfl3S6vzKmlcFI4"  # Your telegram bot token
+DEVELOPER_ID = 6033522354  # Your telegram id (used for error reporting and private mode)
+IS_BOT_PRIVATE = False  # Change this to True to make bot private
