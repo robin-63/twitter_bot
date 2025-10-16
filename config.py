3bf4f4e94bc75924997d31c7135d6a90f1cd0cd9
TomSyex <122930015+robin-63@users.noreply.github.com>
2025-10-16 18:48:29 -0400

    config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "8363099723:AAHZJ4r33wn0YFRKWe1C8gzJ2mFz4916Ix0"  # Your telegram bot token
+BOT_TOKEN = "8459988102:AAH13pYS1Gt_kItWl6tQdES8UmV71UYLbeA"  # Your telegram bot token
 DEVELOPER_ID = 8403480283  # Your telegram id (used for error reporting and private mode)
 IS_BOT_PRIVATE = False  # Change this to True to make bot private
